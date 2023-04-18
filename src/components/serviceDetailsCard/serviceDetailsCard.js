cecc50ece992bb8824ee67fea78bc2606eca1beb
Alex <jvaughn1230@gmail.com>
2023-04-17 19:53:21 -0700

    worked on responsiveness for services page

--- src/components/serviceDetailsCard/serviceDetailsCard.js
+++ src/components/serviceDetailsCard/serviceDetailsCard.js
@@ -1,17 +1,28 @@
 import React from "react";
 import "./serviceDetailsCard.css";
 import { serviceData } from "../../constants/serviceData";
+import { FaArrowRight } from "react-icons/fa";
 
 const ServiceDetailsCard = ({ selectedService }) => {
   const selectedServiceItem = serviceData.find(
     ({ title }) => title === selectedService
   );
 
-  const newServiceDetail = (serviceDetail) => <p>{serviceDetail}</p>;
+  const newServiceDetail = (serviceDetail, idx) => (
+    <p key={idx}>{serviceDetail}</p>
+  );
 
   return (
     <div className="serviceDetails__container">
       {selectedServiceItem.details.map(newServiceDetail)}
+      <div className="pricingContainer__tablet">
+        <span>
+          <h4>Pricing Starts at</h4>
+          <h5>Pricing Here USD</h5>
+        </span>
+
+        <FaArrowRight className="pricingArrow__tablet" />
+      </div>
     </div>
   );
 };
