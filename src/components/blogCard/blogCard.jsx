918a6d54d37812760e0c09d8fdbf8c2fd6952725
Alex <jvaughn1230@gmail.com>
2023-05-08 12:09:27 -0700

    added layers to blog cards

--- src/components/blogCard/blogCard.jsx
+++ src/components/blogCard/blogCard.jsx
@@ -6,6 +6,43 @@ import "./blogCard.css";
 const BlogCard = (post) => {
   const [isHovered, setIsHovered] = useState(false);
   return (
+    <Link to={`/blog/${post.id}`} state={{ post }}>
+      <div className="blogcard gradient-bg">
+        <div
+          className={`blogcard-layer2 ${
+            isHovered ? "transparent" : "cream-bg"
+          }`}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          <div className="blogcard-layer3">
+            <div
+              className={`${isHovered ? "transparent" : "blogcard-bg"}`}
+              style={{
+                backgroundImage: `url('http://localhost:1337${post.img}')`,
+              }}
+            ></div>
+          </div>
+          <div className="blogcard-content">
+            <h3 className={isHovered ? "blogcard__title" : "gradient-text"}>
+              {post.title}
+            </h3>
+
+            <p className={isHovered ? "show blogcard__description" : "hide"}>
+              {post.description}
+            </p>
+            <FaArrowRight
+              className={isHovered ? "show blogcard__arrow" : "hide"}
+              size={60}
+            />
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+
+  /*
+  return (
     <div className="blogcard gradient-bg ">
       <div
         className="background"
@@ -13,6 +50,14 @@ const BlogCard = (post) => {
         onMouseLeave={() => setIsHovered(false)}
       >
         <div className="img-bg">
+          <div className="rd-div">
+            <div
+              className={`${isHovered ? "transparent" : "img-bg"}`}
+              style={{
+                backgroundImage: `url('http://localhost:1337${post.img}')`,
+              }}
+            ></div>
+          </div>
           <div className="blogcard__content">
             <h3 className={isHovered ? "blogcard__title" : "gradient-text"}>
               {post.title}
@@ -33,5 +78,6 @@ const BlogCard = (post) => {
     </div>
   );
 };
-
+*/
+};
 export default BlogCard;
